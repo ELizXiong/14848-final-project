2fd1295b5d05f5a7764a6f97d12950e9ac303ef3
ELizXiong <60122319+ELizXiong@users.noreply.github.com>
2021-10-31 23:44:53 -0400

    added draft for user input GUI

---
+++ src/gui/User_Interface_GUI.py
@@ -0,0 +1,64 @@
+
+"""
+Created on Wed Oct 27 17:56:15 2021
+
+@author: Yiru Xiong
+"""
+#pip install tk
+from tkinter import *
+from tkinter.ttk import Combobox
+
+# create the interface for the toolbox
+gui = Tk()
+label = Label(gui, text="Welcome to Big Data Processing Application", fg='skyblue1', font=("Book Antiqua", 32))
+label.place(x=180, y=60)
+label.configure(bg='lavender')
+button1 = Button(gui, text = "1. Apache Hadoop", fg='palevioletred3', font=("Book Antiqua", 16),height=2, width=15)
+button1.place(x=50, y=150)
+button2 = Button(gui, text = "2. Apache Spark", fg='palevioletred3', font=("Book Antiqua", 16), height=2, width=15)
+button2.place(x=280, y=150)
+button3 = Button(gui, text = "3. Jupyter Notebook", fg='palevioletred3', font=("Book Antiqua", 16), height=2, width=15)
+button3.place(x=510, y=150)
+button4 = Button(gui, text = "4. SonarQube & SonarScanner", fg='palevioletred3', font=("Book Antiqua", 16), height=2, width=22)
+button4.place(x=740, y=150)
+
+# ask for the user input
+# create the list box for user input
+L2 = Label(gui, text="Please select the number that corresponds to the application you would like to run", fg='mediumturquoise', font=("Book Antiqua", 19))
+L2.place(x=30, y=230)
+L2.configure(bg='lavender')
+# select from combobox
+button_list = ["1. Apache Hadoop", "2. Apache Spark", "3. Jupyter Notebook", "4. SonarQube & SonarScanner"]
+optionlist = Combobox(gui, values=button_list, height=20, width=25)
+optionlist.place(x=420, y=260)
+# enter the option
+L2 = Label(gui, text="Or type the number that corresponds to the application you would like to run. Valid Inputs are 1,2,3,4", fg='mediumturquoise', font=("Book Antiqua", 19))
+L2.place(x=30, y=390)
+L2.configure(bg='lavender')
+var= StringVar()
+
+user_entry = Entry(gui, textvariable=var, bd=2)
+user_entry.pack()
+user_input = user_entry.get()
+user_entry.place(x=450,y=420)
+
+def submit():
+    global user_entry
+    global input_string
+    input_string = user_entry.get()
+    print("user input is:", input_string)
+
+submit_button = Button(gui, text = "Submit", fg='mediumturquoise', font=("Book Antiqua", 16),height=2, width=6,command=lambda: submit())
+submit_button.pack()
+submit_button.place(x=510, y=450)
+#for bl in button_list:
+#    optionlist.insert(END, bl)
+gui.title('Data Science Toolbox v.1.5.1')
+gui.geometry("500x500+11+11")
+gui.configure(bg='lavender')
+
+gui.mainloop()
+
+
+
+    
