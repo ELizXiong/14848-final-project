9c8b6902f8f99650759c38c11b7356160b371c2f
ELizXiong <60122319+ELizXiong@users.noreply.github.com>
2021-11-28 21:42:28 -0500

    updated user interface GUI

--- src/gui/User_Interface_GUI.py
+++ src/gui/User_Interface_GUI.py
@@ -1,12 +1,14 @@
-
+#!/usr/bin/env python3
 """
 Created on Wed Oct 27 17:56:15 2021
 
 @author: Yiru Xiong
 """
-#pip install tk
+#!pip install tk
 from tkinter import *
 from tkinter.ttk import Combobox
+import webbrowser
+
 
 # create the interface for the toolbox
 gui = Tk()
@@ -31,6 +33,21 @@ L2.configure(bg='lavender')
 button_list = ["1. Apache Hadoop", "2. Apache Spark", "3. Jupyter Notebook", "4. SonarQube & SonarScanner"]
 optionlist = Combobox(gui, values=button_list, height=20, width=25)
 optionlist.place(x=420, y=260)
+confirm_button = Button(gui, text = "confirm", fg='mediumturquoise', font=("Book Antiqua", 16),height=2, width=6,command=lambda: confirm())
+confirm_button.pack()
+confirm_button.place(x=510, y=300)
+def confirm():
+    if optionlist.get() == "1. Apache Hadoop":
+        webbrowser.open('http://35.239.106.172:9870/')
+    elif optionlist.get() == "2. Apache Spark":
+        webbrowser.open('http://34.132.229.113:8080/')
+    elif optionlist.get() == "3. Jupyter Notebook":
+        webbrowser.open('http://35.232.118.254:8888/')
+    elif optionlist.get() == "4. SonarQube & SonarScanner":
+        webbrowser.open('http://104.197.148.211:9000/')
+    else:
+        return
+
 # enter the option
 L2 = Label(gui, text="Or type the number that corresponds to the application you would like to run. Valid Inputs are 1,2,3,4", fg='mediumturquoise', font=("Book Antiqua", 19))
 L2.place(x=30, y=390)
@@ -42,15 +59,30 @@ user_entry.pack()
 user_input = user_entry.get()
 user_entry.place(x=450,y=420)
 
+# valid input: 1,2,3,4
+app_nums = ["1","2","3","4"]
 def submit():
     global user_entry
     global input_string
     input_string = user_entry.get()
-    print("user input is:", input_string)
+    if input_string not in app_nums:
+    	print("Please enter a valid application number")
+    else:
+        print("user input is:", input_string)
+        if (input_string == "1"):
+        	webbrowser.open('http://35.239.106.172:9870/')
+        elif (input_string == "2"):
+        	webbrowser.open('http://34.132.229.113:8080/')
+        elif (input_string == "3"):
+        	webbrowser.open('http://35.232.118.254:8888/')
+        elif (input_string == "4"):
+        	webbrowser.open('http://104.197.148.211:9000/')
+        else:
+        	return
 
 submit_button = Button(gui, text = "Submit", fg='mediumturquoise', font=("Book Antiqua", 16),height=2, width=6,command=lambda: submit())
 submit_button.pack()
-submit_button.place(x=510, y=450)
+submit_button.place(x=510, y=460)
 #for bl in button_list:
 #    optionlist.insert(END, bl)
 gui.title('Data Science Toolbox v.1.5.1')
